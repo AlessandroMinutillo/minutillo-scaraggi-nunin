712f7c910c6610d164f4fd17319a9c236a62d081
Vito-Scaraggi <s1092648@studenti.univpm.it>
2020-12-13 17:23:23 +0100

    start

---
+++ weather_app/src/main/java/msn/weather_app/WeatherAppApplication.java
@@ -0,0 +1,13 @@
+package msn.weather_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAppApplication.class, args);
+	}
+
+}
