894ca6983303c99ad2c033d4fae64f0be2a8366e
Vito-Scaraggi <s1092648@studenti.univpm.it>
2020-12-13 17:42:51 +0100

    messaggio di prova

--- weather_app/src/main/java/msn/weather_app/WeatherAppApplication.java
+++ weather_app/src/main/java/msn/weather_app/WeatherAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WeatherAppApplication {
-
+	//questo è un commento
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherAppApplication.class, args);
 	}
