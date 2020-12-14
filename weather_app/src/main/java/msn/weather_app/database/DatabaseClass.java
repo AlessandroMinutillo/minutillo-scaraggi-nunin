19f5bd2aaae6fe27ec7100b7f8016ce540fae45c
Alessandro Minutillo <S1092465@studenti.univpm.it>
2020-12-14 17:09:54 +0100

    Modifica della metadata.class

--- weather_app/src/main/java/msn/weather_app/database/DatabaseClass.java
+++ weather_app/src/main/java/msn/weather_app/database/DatabaseClass.java
@@ -7,8 +7,19 @@ public class DatabaseClass {
 	private static ArrayList<Metadata> meta = new ArrayList<Metadata>();
 	
 	public static ArrayList<Metadata> getMeta(){
-		meta.add(new Metadata("temp","temperatura","String"));
-		meta.add(new Metadata("press","pressione","String"));
+		meta.add(new Metadata("CityName","Nome della citta'","String"));
+		meta.add(new Metadata("Lat","Latitudine","Double"));
+		meta.add(new Metadata("Lon","Longitudine","Double"));
+		meta.add(new Metadata("TempAtt","Temperatura attuale (°C)","Double"));
+		meta.add(new Metadata("PressAtt","Pressione attuale (hPa)","Double"));
+		meta.add(new Metadata("TempMin","Temperatura minima (°C)","Double"));
+		meta.add(new Metadata("TempMax","Temperatura massima (°C)","Double"));
+		meta.add(new Metadata("PressMin","Pressione minima (hPa)","Double"));
+		meta.add(new Metadata("PressMax","Pressione massima (hPa)","Double"));
+		meta.add(new Metadata("TempMedia","Media della temperatura (°C)","Double"));
+		meta.add(new Metadata("TempVar","Varianza della temperatura (°C)","Double"));
+		meta.add(new Metadata("PressMedia","Media della pressione (hPa)","Double"));
+		meta.add(new Metadata("PressVar","Varianza della pressione (hPa)","Double"));
 		return meta;
 	}
 }
